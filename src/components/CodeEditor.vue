2fca77a8e74feb480138cd0ce2983eebbed3f16f
Max Milton <max@wearegenki.com>
2017-10-14 16:46:17 +1100

    Add new code editor component + remove unnecessary NPM packages

---
+++ src/components/CodeEditor.vue
@@ -0,0 +1,132 @@
+<!--
+  CODE EDITOR
+
+  README:
+    Simple code editor base on the CodeMirror library. Caution: this is a heavyweight
+    component as its dependencies add around 61 KB to the overall bundle size.
+
+  USAGE:
+    <code-editor v-model="code" mode="text/javascript"/>
+
+    <code-editor v-model="code"
+      mode="application/json"
+      :read-only="true"
+      :match-brackets="false"
+      :style-active-line="false"
+    />
+-->
+<template>
+<textarea :readonly="readOnly"></textarea>
+</template>
+
+<script>
+import CodeMirror from 'codemirror/lib/codemirror';
+import 'codemirror/mode/javascript/javascript';
+// import 'codemirror/addon/lint/lint';
+// import jsonlint from 'jsonlint';
+// import 'codemirror/addon/lint/json-lint';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/selection/active-line.js';
+
+// window.jsonlint = jsonlint;
+
+export default {
+  name: 'code-editor',
+  props: {
+    mode: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      default: '',
+    },
+    readOnly: {
+      type: Boolean,
+      default: false,
+    },
+    lineNumbers: {
+      type: Boolean,
+      default: true,
+    },
+    lineWrapping: {
+      type: Boolean,
+      default: true,
+    },
+    matchBrackets: {
+      type: Boolean,
+      default: true,
+    },
+    styleActiveLine: {
+      type: Boolean,
+      default: true,
+    },
+    // lint: {
+    //   type: Boolean,
+    //   default: true,
+    // },
+  },
+  data() {
+    return {
+      skipNextChangeEvent: false,
+    };
+  },
+  watch: {
+    // value(newVal, oldVal) {
+    value(newVal) {
+      const editorValue = this.editor.getValue();
+
+      if (newVal !== editorValue) {
+        this.skipNextChangeEvent = true;
+        const scrollInfo = this.editor.getScrollInfo();
+        this.editor.setValue(newVal);
+        this.editor.scrollTo(scrollInfo.left, scrollInfo.top);
+      }
+    },
+  },
+  mounted() {
+    this.editor = CodeMirror.fromTextArea(this.$el, {
+      mode: this.mode,
+      readOnly: this.readOnly ? 'nocursor' : false,
+      lineNumbers: this.lineNumbers,
+      lineWrapping: this.lineWrapping,
+      matchBrackets: this.matchBrackets,
+      styleActiveLine: this.styleActiveLine,
+      // lint: this.lint,
+      // gutters: ['CodeMirror-lint-markers'],
+      theme: 'dracula',
+      tabSize: 2,
+      viewportMargin: Infinity,
+      autoCloseBrackets: true,
+      // hint: true,
+    });
+    this.editor.setValue(this.value);
+    this.editor.on('change', (cm) => {
+      if (this.skipNextChangeEvent) {
+        this.skipNextChangeEvent = false;
+        return;
+      }
+      this.$emit('input', cm.getValue());
+    });
+  },
+  beforeDestroy() {
+    if (this.editor) {
+      this.editor.toTextArea();
+    }
+  },
+};
+</script>
+
+<style>
+@import 'codemirror/lib/codemirror';
+@import 'codemirror/theme/dracula';
+/* @import 'codemirror/addon/lint/lint'; */
+@import 'css/import';
+
+.CodeMirror {
+  min-width: 100%;
+  height: auto;
+  box-shadow: var(--shadow-subtle);
+}
+</style>
