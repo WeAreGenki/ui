e3a6412f254a26326905d17258fbe2e7e2f8c560
Max Milton <max@wearegenki.com>
2017-10-27 09:18:26 +1100

    New components (WIP)

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,28 @@
+<template>
+<span class="dropdown">
+  <button class="btn btn-clear ml-auto" title="Org options">
+    <svg class="icon-small"><use :xlink:href="`~@/assets/icons/${ icon }.svg`"/></svg>
+  </button>
+</span>
+</template>
+
+<script>
+export default {
+  name: 'dropdown',
+  props: {
+    icon: {
+      type: String,
+      default: 'more-vertical',
+    },
+  },
+  // data() {
+  //   return {};
+  // },
+};
+</script>
+
+<style>
+/* @import "css/import"; */
+
+/* #dropdown {} */
+</style>
---
+++ src/components/InputToggle.vue
@@ -0,0 +1,32 @@
+<template>
+<span @click="toggle" class="input-toggle">
+  <svg v-if="value" class="icon"><use xlink:href="~@/assets/icons/check-square.svg"/></svg>
+  <svg v-else class="icon"><use xlink:href="~@/assets/icons/square.svg"/></svg>
+
+  <svg v-if="value" class="icon"><use xlink:href="~@/assets/icons/toggle-left.svg"/></svg>
+  <svg v-else class="icon"><use xlink:href="~@/assets/icons/toggle-right.svg"/></svg>
+</span>
+</template>
+
+<script>
+export default {
+  name: 'input-toggle',
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    toggle() {
+      this.$emit('input', !this.value);
+    },
+  },
+};
+</script>
+
+<style>
+/* @import "css/import"; */
+
+/* .input-toggle {} */
+</style>
