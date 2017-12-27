6824e20e288e5911766d28dd76220f8f44338043
Max Milton <max@wearegenki.com>
2017-12-28 01:24:54 +1100

    CSS build debug improvements; dynamically load PostCSS config, error and souce map handling

--- debug-postcss.js
+++ debug-postcss.js
@@ -1,42 +1,47 @@
 // https://github.com/andywer/postcss-debug#js-code
 
-/* eslint-disable */
+/* eslint-disable strict, no-console */
+
+'use strict';
 
 const fs = require('fs');
-const path = require('path');
 const postcss = require('postcss');
-const { createDebugger, matcher } = require('postcss-debug');
+const postcssrc = require('postcss-load-config');
+const { createDebugger } = require('postcss-debug');
 
 const debug = createDebugger();
-// // or limit gathering debug data to certain css files only:
-// const debug = createDebugger([
-//   matcher.contains('style/some-file.css'),
-//   matcher.regex(/foo\.css/)
-// ])
 
-
-const plugins = [
-  require('postcss-import')({ path: ['src', 'node_modules'] }),
-  require('postcss-at-rules-variables'),
-  require('postcss-each'),
-  require('postcss-mixins')({
-    mixinsDir: [path.join(__dirname, 'src/css/mixins')],
-  }),
-  require('postcss-nested'),
-  require('postcss-css-variables'),
-  require('postcss-conditionals'),
-  require('postcss-custom-media'),
-  require('postcss-calc')({ warnWhenCannotResolve: true }),
-  require('postcss-color-function'),
-  require('css-mqpacker'),
-  require('autoprefixer')({ remove: false }),
-];
-
-const opts = {
+const ctx = {
+  map: { inline: false },
   from: './src/css/main.css',
   to: './lib/index.css',
 };
 
-postcss(debug(plugins))
-  .process(fs.readFileSync(opts.from, 'utf-8'), opts)
-  .then(result => { debug.inspect(); });
+const css = fs.readFileSync(ctx.from, 'utf8');
+
+postcssrc(ctx).then(({ plugins, options }) => {
+  postcss(debug(plugins))
+    .process(css, options)
+    .then((result) => {
+      result.warnings().forEach((warn) => {
+        process.stderr.write(`${warn.toString()}\n`);
+      });
+
+      if (result.map) {
+        fs.writeFile(`${result.opts.to}.map`, result.map.toString());
+      }
+
+      debug.inspect();
+      console.info('\n-----------------------------------------\n');
+      console.info(plugins);
+      console.info('\n-----------------------------------------\n');
+      console.info(result);
+    })
+    .catch((error) => {
+      if (error.name === 'CssSyntaxError') {
+        process.stderr.write(error.message + error.showSourceCode());
+      } else {
+        throw error;
+      }
+    });
+});
