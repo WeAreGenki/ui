aa1c0fec2ddf37948ef1ca70ea1db763d763bc40
Max Milton <max@wearegenki.com>
2017-12-28 01:26:37 +1100

    Improve CSS build flow for better performance

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,24 +1,30 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
+// TODO: Create a new package with our PostCSS packages + config
+//  ↳ @wearegenki/ui-postcss
+
 const path = require('path');
 
 module.exports = {
   plugins: {
-    'postcss-import': { path: ['src', 'node_modules'] },
+    // 'postcss-use': {
+    //   modules: ['postcss-import', '*'],
+    //   options: {
+    //     'postcss-import': { path: ['src'] },
+    //   },
+    // },
+    'postcss-import': { path: ['src'] },
     'postcss-at-rules-variables': {},
     'postcss-each': {},
-    'postcss-mixins': {
-      mixinsDir: [path.join(__dirname, 'src/css/mixins')]
-    },
+    'postcss-mixins': { mixinsDir: path.join(__dirname, 'src/css/mixins') },
     'postcss-nested': {},
-    'postcss-css-variables': {},
+    'postcss-custom-properties': {},
     'postcss-conditionals': {},
     'postcss-custom-media': {},
     'postcss-calc': { warnWhenCannotResolve: true },
     'postcss-color-function': {},
+    // 'css-mqpacker': { sort: true }, // TODO: Experiment with media query order, is it correct without this?
     'css-mqpacker': {},
-    'autoprefixer': { remove: false },
-    // FIXME: Get reporter messages in webpack errors
-    'postcss-reporter': { filter: msg => msg.type !== 'dependency' },
-  }
+    autoprefixer: { cascade: false, remove: false, grid: true },
+  },
 };
