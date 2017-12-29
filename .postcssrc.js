1fae18eef1ab7f53b6dd8263272f170a8b6eace9
Max Milton <max@wearegenki.com>
2017-12-30 07:04:05 +1100

    Use the postcss preset for our own internal use too

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,23 +1,23 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
-const path = require('path');
+// const { uiPostcssPreset } = require('@wearegenki/ui');
+
+// module.exports = {
+//   plugins: [
+//     uiPostcssPreset({
+//       // importPath: ['src'],
+//       // mixinsDir: 'src/css/mixins',
+//       // verbose: true,
+//     }),
+//   ],
+// };
 
 module.exports = {
   plugins: {
-    'postcss-import': { path: ['packages/css/src'] },
-    'postcss-at-rules-variables': {},
-    'postcss-each': {},
-    'postcss-mixins': {
-      mixinsDir: [path.join(__dirname, 'packages/css/src/mixins')],
+    '@wearegenki/ui-postcss': {
+      // importPath: ['src'],
+      // mixinsDir: 'src/css/mixins',
+      // verbose: true,
     },
-    'postcss-nested': {},
-    'postcss-custom-properties': {},
-    'postcss-conditionals': {},
-    'postcss-custom-media': {},
-    // 'postcss-calc': { warnWhenCannotResolve: true },
-    'postcss-calc': {},
-    'postcss-color-function': {},
-    'css-mqpacker': {},
-    autoprefixer: { cascade: false, remove: false, grid: true },
   },
 };
