922382856f0780defcd50090fbed11d94806f297
Max Milton <max@wearegenki.com>
2017-12-30 07:14:40 +1100

    Clean up postcss config

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,23 +1,5 @@
-// https://github.com/michael-ciniawsky/postcss-load-config
-
-// const { uiPostcssPreset } = require('@wearegenki/ui');
-
-// module.exports = {
-//   plugins: [
-//     uiPostcssPreset({
-//       // importPath: ['src'],
-//       // mixinsDir: 'src/css/mixins',
-//       // verbose: true,
-//     }),
-//   ],
-// };
-
 module.exports = {
   plugins: {
-    '@wearegenki/ui-postcss': {
-      // importPath: ['src'],
-      // mixinsDir: 'src/css/mixins',
-      // verbose: true,
-    },
+    '@wearegenki/ui-postcss': {},
   },
 };
