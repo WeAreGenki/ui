ec192c643a78433addc274e91085f312002ce68e
Max Milton <max@wearegenki.com>
2017-12-30 02:48:34 +1100

    Create new poscss preset pack + rename it from ui-build-postcss to ui-postcss

---
+++ packages/ui-postcss/index.js
@@ -0,0 +1,40 @@
+'use-strict';
+
+const path = require('path');
+const postcss = require('postcss');
+const cssImport = require('postcss-import');
+const atRulesVars = require('postcss-at-rules-variables');
+const each = require('postcss-each');
+const mixins = require('postcss-mixins');
+const nested = require('postcss-nested');
+const customProperties = require('postcss-custom-properties');
+const conditionals = require('postcss-conditionals');
+const customMedia = require('postcss-custom-media');
+const calc = require('postcss-calc');
+const colorFunction = require('postcss-color-function');
+const mediaQueryPacker = require('css-mqpacker');
+const autoprefixer = require('autoprefixer');
+
+const mixinsPath = path.join(
+  path.dirname(require.resolve('@wearegenki/css')),
+  'src/mixins',
+);
+
+module.exports = postcss.plugin('uiPostcssPreset', (opts = {}) => {
+  const mixinsDir = [opts.mixinsDir] || [];
+  mixinsDir.push(mixinsPath);
+
+  return postcss()
+    .use(cssImport({ path: opts.importPath || ['src'] }))
+    .use(atRulesVars)
+    .use(each)
+    .use(mixins({ mixinsDir }))
+    .use(nested)
+    .use(customProperties)
+    .use(conditionals)
+    .use(customMedia)
+    .use(calc({ warnWhenCannotResolve: opts.verbose }))
+    .use(colorFunction)
+    .use(mediaQueryPacker)
+    .use(autoprefixer({ cascade: false, remove: false, grid: true }));
+});
---
+++ packages/ui/index.js
@@ -0,0 +1,7 @@
+'use-strict';
+
+const uiPostcssPreset = require('@wearegenki/ui-postcss');
+
+module.exports = {
+  uiPostcssPreset,
+};
