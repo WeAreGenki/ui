a6f9255f268d8b17d2820aa0cc32a18cee3f778f
Max Milton <max@wearegenki.com>
2017-12-31 16:18:34 +1100

    Add comments about ui-postcss in docs

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,3 +3,29 @@ module.exports = {
     '@wearegenki/ui-postcss': {},
   },
 };
+
+// TODO: Put this in the docs (both ways, depending on array or object type of config):
+
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+// const { uiPostcssPreset } = require('@wearegenki/ui');
+
+// module.exports = {
+//   plugins: [
+//     uiPostcssPreset({
+//       // importPath: ['src'],
+//       mixinsDir: 'src/css/mixins',
+//       // verbose: true,
+//     }),
+//   ],
+// };
+
+// module.exports = {
+//   plugins: {
+//     '@wearegenki/ui-postcss': {
+//       // importPath: ['src'],
+//       mixinsDir: 'src/css/mixins',
+//       // verbose: true,
+//     },
+//   },
+// };
