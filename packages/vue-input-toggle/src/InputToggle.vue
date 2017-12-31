0a7d1a8519b4becfd935331b0af5c7e0901241d7
Max Milton <max@wearegenki.com>
2017-12-31 19:34:19 +1100

    Better spacing of elements in input toggle

--- packages/vue-input-toggle/src/InputToggle.vue
+++ packages/vue-input-toggle/src/InputToggle.vue
@@ -1,10 +1,18 @@
 <template>
 <span @click="toggle" class="input-toggle">
-  <svg v-if="value" class="icon"><use xlink:href="~@wearegenki/icons/src/check-square.svg"/></svg>
-  <svg v-else class="icon"><use xlink:href="~@wearegenki/icons/src/square.svg"/></svg>
+  <svg v-if="value" class="icon">
+    <use xlink:href="~@wearegenki/icons/src/check-square.svg"/>
+  </svg>
+  <svg v-else class="icon">
+    <use xlink:href="~@wearegenki/icons/src/square.svg"/>
+  </svg>
 
-  <svg v-if="value" class="icon"><use xlink:href="~@wearegenki/icons/src/toggle-left.svg"/></svg>
-  <svg v-else class="icon"><use xlink:href="~@wearegenki/icons/src/toggle-right.svg"/></svg>
+  <svg v-if="value" class="icon">
+    <use xlink:href="~@wearegenki/icons/src/toggle-left.svg"/>
+  </svg>
+  <svg v-else class="icon">
+    <use xlink:href="~@wearegenki/icons/src/toggle-right.svg"/>
+  </svg>
 </span>
 </template>
 
