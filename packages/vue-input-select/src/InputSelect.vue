a6dbd9b011c1ca3a989eca50b4fe213076cc403b
Max Milton <max@wearegenki.com>
2018-01-04 11:31:36 +1100

    Get the select component to a usable state; better events, animation, props, etc.

--- packages/vue-input-select/src/InputSelect.vue
+++ packages/vue-input-select/src/InputSelect.vue
@@ -1,57 +1,66 @@
-<!-- FIXME: Skip over disabled option when using keyboard input -->
-<!-- TODO: Check all events etc. are actually necessary -->
-<!-- TODO: Add animation, something like Google play music search -->
-
 <!--
-  TODO: Add documentation, especially options stucture:
-  [
-    { id: 'ex1', text: 'Example 1' },
-    { id: 'ex2', text: 'Example 2' },
-    { id: 'ex3', text: 'Example 3', disabled: true },
-  ]
+  INPUT SELECT COMPONENT
+
+  README:
+    TODO: Write me.
+
+    Features:
+      - Automatically opens on focus for taster form input when using a keyboard for navigating.
+
+  USAGE:
+    TODO: Add documentation, especially options stucture.
+
+    [
+      { id: 'ex1', text: 'Example 1' },
+      { id: 'ex2', text: 'Example 2' },
+      { id: 'ex3', text: 'Example 3', disabled: true },
+    ]
+
 -->
 
 <template>
-  <div class="pos-r dif f-col" :class="{ 'input-select-active': active }">
+  <div class="pos-r dif f-col">
     <input
       @click="active ? false : open()"
       @focus="open"
-      @blur="blur"
+      @blur="close"
       @keydown.space="active ? false : open()"
-      @keydown.enter.prevent="active ? select('enter') : toggle()"
+      @keydown.enter.prevent="active ? select('enter') : open()"
       @keydown.esc="close"
-      @keydown.up="up"
-      @keydown.down="active ? down() : open()"
-      v-model="text"
+      @keydown.up.prevent="active ? up() : open()"
+      @keydown.down.prevent="active ? down() : open()"
+      v-model="valueText"
       class="select"
-      :tabindex="this.$attrs.disabled === undefined ? 0 : -1"
+      :tabindex="disabled ? -1 : 0"
       role="listbox"
-      placeholder="Search..."
-      :readonly="this.$attrs.readonly || !active"
-      :disabled="this.$attrs.disabled !== undefined"
+      :placeholder="placeholder"
+      :readonly="readonly || !active"
+      :disabled="disabled"
     >
-    <span class="input-select-caret"/>
+    <span class="input-select-caret" :class="{ 'input-select-active': active }"/>
 
-    <div
-      v-show="active"
-      @mousedown.prevent="select"
-      class="input-select-dropdown w-100 z1"
-    >
-      <option
-        v-for="(option, index) in list"
-        :key="option.id"
-        :data-id="option.id"
-        class="input-select-option bg-alt"
-        :class="{ 'selected': index === selected}"
-        role="option"
-        :disabled="option.disabled"
+    <transition name="input-select">
+      <div
+        v-show="active"
+        @mousedown.prevent="select"
+        class="input-select-dropdown w-100 z5"
       >
-        {{ option.text }}
-      </option>
-      <div v-if="!list.length" class="pv2 ph3 grey">
-        No matches
+        <option
+          v-for="(option, index) in list"
+          :key="option.id"
+          :data-id="option.id"
+          class="input-select-option"
+          :class="{ 'selected': index === i}"
+          role="option"
+          :disabled="option.disabled"
+        >
+          {{ option.text }}
+        </option>
+        <div v-if="!list.length" class="pv2 ph3 grey">
+          No matches
+        </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -67,87 +76,121 @@ export default {
       type: Array,
       required: true,
     },
+    placeholder: { // for search input
+      type: String,
+      default: 'Filter...',
+    },
+    readonly: {
+      type: Boolean,
+      default: undefined,
+    },
+    disabled: {
+      type: Boolean,
+      default: undefined,
+    },
   },
   data: () => ({
     active: false,
-    searchText: '',
-    selected: 0,
+    filter: '',
+    i: 0,
   }),
   computed: {
     list() {
-      const search = this.searchText.toLowerCase();
-      return this.options.filter(option => option.text.toLowerCase().indexOf(search) > -1);
+      const search = this.filter.toLowerCase();
+      const list = this.options.filter(option => option.text.toLowerCase().indexOf(search) > -1);
+      this.setIndex(list);
+      return list;
     },
-    text: {
+    valueText: {
       get() {
         if (this.active) {
-          return this.searchText;
+          return this.filter;
         }
 
-        const current = this.options.find((option, index) => {
-          this.selected = index; // eslint-disable-line
-          return option.id === this.value;
-        });
+        const current = this.options.find(option => option.id === this.value);
         return current.text;
       },
       set(value) {
-        this.searchText = value;
+        this.filter = value;
       },
     },
   },
   methods: {
+    setIndex(list = this.list) {
+      // save the current item's index so we can highlight it in the list
+      this.i = list.findIndex(option => option.id === this.value);
+    },
     open() {
       if (this.$attrs.disabled === undefined) {
+        this.setIndex();
         this.active = true;
       }
     },
     close() {
       this.active = false;
-    },
-    toggle() {
-      if (this.active) {
-        this.close();
-      } else {
-        this.open();
-      }
+      this.filter = '';
     },
     select(event) {
-      if (event.target && !event.target.disabled && event.target.dataset.id) {
+      if (
+        event.target
+        && !event.target.disabled
+        && event.target.dataset.id
+      ) {
         this.$emit('input', event.target.dataset.id);
         this.close();
-      } else if (event === 'enter' && !this.options[this.selected].disabled) {
-        this.$emit('input', this.options[this.selected].id);
+      } else if (event === 'enter' && !this.list[this.i].disabled) {
+        this.$emit('input', this.list[this.i].id);
         this.close();
       }
     },
-    blur() {
-      this.close();
-    },
     up() {
-      this.selected -= 1;
+      if (this.i <= 0) return;
+
+      let steps = 1;
+
+      while (this.list[this.i - steps].disabled) {
+        steps += 1;
+        if (this.list[this.i - steps] === undefined) return;
+      }
+
+      this.i -= steps;
     },
     down() {
-      this.selected += 1;
+      // jump to last availiable item if index is out of bounds (e,g, after filtering)
+      if (this.i >= this.list.length - 1) this.i = this.list.length - 1;
+
+      let steps = 1;
+
+      while (this.list[this.i + steps].disabled) {
+        steps += 1;
+        if (this.list[this.i + steps] === undefined) return;
+      }
+
+      this.i += steps;
     },
   },
 };
 </script>
 
 <style>
-@import "css/import";
+@import "@wearegenki/ui/import";
 
 .input-select-caret {
   position: absolute;
-  top: calc(50% - 2px);
+  top: calc(50% - (var(--input-select-caret-size) / 2));
   right: 0.75rem;
   width: 0;
   height: 0;
-  border-top: 0.3rem solid var(--blue-grey-800);
-  border-right: 0.3rem solid transparent;
-  border-left: 0.3rem solid transparent;
+  border-top: var(--input-select-caret-size) solid var(--input-select-caret-colour);
+  border-right: var(--input-select-caret-size) solid transparent;
+  border-left: var(--input-select-caret-size) solid transparent;
 
-  .input-select-active > & {
+  &.input-select-active {
     transform: rotate(180deg);
+  }
+
+  [disabled] + & {
+    border-top-color: var(--input-select-caret-colour-disabled);
   }
 }
 
@@ -159,16 +202,29 @@ export default {
   color: var(--body-colour);
   background-color: var(--white);
   box-shadow: var(--shadow);
+  transition:
+    transform var(--input-select-animate-speed) ease,
+    opacity var(--input-select-animate-speed) ease;
+  will-change: transform, opacity;
+
+  &.input-select-enter,
+  &.input-select-leave-to {
+    opacity: 0;
+    transform: translateY(-1rem);
+  }
 }
 
 .input-select-option {
   padding: var(--input-padding-y) var(--input-padding-x);
 
   &.selected,
-  &:hover,
-  &:focus {
-    color: var(--white);
-    background-color: var(--link-hover-colour);
+  &:not([disabled]):hover {
+    color: var(--input-select-selected-colour);
+    background-color: var(--input-select-selected-bg-colour);
+  }
+
+  &[disabled] {
+    color: var(--input-select-colour-disabled);
   }
 }
 </style>
