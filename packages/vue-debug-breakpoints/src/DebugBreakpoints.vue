92365159d303f67365df87c4f4203096ce4d1576
Max Milton <max@wearegenki.com>
2018-01-12 17:03:46 +1100

    Update debug breakpoints component to new breakpoints + clean up

--- packages/vue-debug-breakpoints/src/DebugBreakpoints.vue
+++ packages/vue-debug-breakpoints/src/DebugBreakpoints.vue
@@ -1,49 +1,33 @@
 <template>
-  <div id="debug-breakpoints">
-    <dl class="code mv0">
-      <dt>xs</dt>
-      <dd :class="xs ? 'green' : 'red'">{{ xs }}</dd>
-      <dt>s</dt>
-      <dd :class="s ? 'green' : 'red'">{{ s }}</dd>
-      <dt>ns</dt>
-      <dd :class="ns ? 'green' : 'red'">{{ ns }}</dd>
-      <dt>m</dt>
-      <dd :class="m ? 'green' : 'red'">{{ m }}</dd>
-      <dt>l</dt>
-      <dd :class="l ? 'green' : 'red'">{{ l }}</dd>
-    </dl>
-  </div>
+  <dl class="pos-f r0 b0 dg x2 compact pv2 ph3 mv0 white bold" id="breakpoints">
+    <dt>none</dt>
+    <dd class="ml2" :class="none ? 'green' : 'red'">{{ none }}</dd>
+    <dt>s</dt>
+    <dd class="ml2" :class="s ? 'green' : 'red'">{{ s }}</dd>
+    <dt>l</dt>
+    <dd class="ml2" :class="l ? 'green' : 'red'">{{ l }}</dd>
+  </dl>
 </template>
 
 <script>
-// Define breakpoints
-// TODO: Can these be extracted from the CSS custom media variables?
-const xs = matchMedia('(max-width: 29.999em)');
-const s = matchMedia('(min-width: 30em) and (max-width: 48em)');
-const m = matchMedia('(min-width: 48.01em) and (max-width: 64em)');
-const l = matchMedia('(min-width: 64.01em)');
+const none = matchMedia('(max-width: 30em)');
+const s = matchMedia('(min-width: 30.01em) and (max-width: 48em)');
+const l = matchMedia('(min-width: 48.01em)');
 
 export default {
   name: 'DebugBreakpoints',
   data: () => ({
-    xs: xs.matches,
+    none: none.matches,
     s: s.matches,
-    m: m.matches,
     l: l.matches,
   }),
-  computed: {
-    ns() {
-      return this.m || this.l;
-    },
-  },
   mounted() {
     this.setListeners();
   },
   methods: {
     setListeners() {
-      xs.addListener((event) => { this.xs = event.matches; });
+      none.addListener((event) => { this.none = event.matches; });
       s.addListener((event) => { this.s = event.matches; });
-      m.addListener((event) => { this.m = event.matches; });
       l.addListener((event) => { this.l = event.matches; });
     },
   },
@@ -51,15 +35,7 @@ export default {
 </script>
 
 <style>
-@import "@wearegenki/ui/import";
-
-#debug-breakpoints { /* stylelint-disable-line selector-max-id */
-  position: fixed;
-  right: 0;
-  bottom: 0;
-  padding: 0.5rem 1rem;
-  font-weight: 700;
-  color: var(--white);
+#breakpoints { /* stylelint-disable-line selector-max-id */
   background-color: rgba(0, 0, 0, 0.6);
 }
 </style>
