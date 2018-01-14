2f416f698e02359907e68aeda37753fe42fead89
Max Milton <max@wearegenki.com>
2018-01-14 11:16:03 +1100

    Update custom builds example (fully working, just need a better readme)

---
+++ docs/examples/custom-builds/.postcssrc.js
@@ -0,0 +1,30 @@
+/**
+ * XXX: Customised PostCSS build configuration for standalone builds. When using
+ *  a JavaScript bundler you typically don't need many of these options as your
+ *  bundler will take care of these things — you can use a simpler configuration
+ *  like the the commented out example below.
+ */
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  map: { inline: !isProduction }, // create an external source map in production builds
+  plugins: {
+    '@wearegenki/ui-postcss': {
+      mixinsDir: 'src/css/mixins', // Optional, if you're using custom mixins
+    },
+    cssnano: isProduction ? {} : false, // optimise CSS in production builds
+  },
+};
+
+// ---------------------------------------------------------------------------- //
+
+// XXX: Use this simple configuration when using a JS bundler like Webpack:
+
+// module.exports = {
+//   plugins: {
+//     '@wearegenki/ui-postcss': {
+//       mixinsDir: 'src/css/mixins', // Optional, if you're using custom mixins
+//     },
+//   },
+// };
--- docs/examples/custom-builds/README.md
+++ docs/examples/custom-builds/README.md
@@ -1,6 +1,40 @@
 # Custom CSS Builds
 
-TODO: Write me!
+This example covers a full _standalone_ custom CSS build workflow. A similar setup can be used for builds with a JavaScript bundler like Webpack or Rollup — replace `postcss-cli` with your bundler specific postcss runner, e.g. `postcss-loader` or `rollup-plugin-postcss`.
+
+## Usage
+
+Install this project's dependencies (on the command line, in this directory):
+
+```bash
+npm install
+# OR
+yarn
+```
+
+### Development
+
+Compile the CSS then watch for and rebuild on changes:
+
+```bash
+npm start
+# OR
+yarn start
+```
+
+### Production
+
+Compile all the CSS:
+
+```bash
+npm run build
+# OR
+yarn run build
+```
+
+## Instructions
+
+TODO: Write me! This section should explain ALL the things!
 
 -----
 
