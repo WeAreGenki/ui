a5df0df69e954aadf5c31e058442f11d7bdb5839
Max Milton <max@wearegenki.com>
2018-01-16 16:48:30 +1100

    Tweak eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,13 +7,14 @@ module.exports = {
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: 'babel-eslint',
-    ecmaVersion: 2017,
+    ecmaVersion: 8,
     sourceType: 'module',
     ecmaFeatures: {
       experimentalObjectRestSpread: true
     },
   },
   env: {
+    es6: true,
     browser: true,
   },
   plugins: [
@@ -30,7 +31,7 @@ module.exports = {
     'import/resolver': {
       node: { extensions: ['.js', '.vue', '.marko', '.json'] }
     },
-    'html/html-extensions': ['.html'], // only lint .html files with this plugin not .vue
+    'html/html-extensions': ['.html'], // only lint html files (not .vue, .marko, etc.)
   },
   rules: {
     'import/extensions': ['error', 'always', {
@@ -48,22 +49,19 @@ module.exports = {
       optionalDependencies: ['test/unit/index.js']
     }],
     'no-debugger': isProduction ? 'error' : 'off',
-    // 'compat/compat': 'warn', // find out which features need a polyfill
+    // 'compat/compat': 'warn', // find out which features need a polyfill // TODO: Load this only via a separate an NPM script
     'no-underscore-dangle': 'off', // needed for @wearegenki/db
-    'max-len': 'off', // FIXME: Currently broken with .vue template blocks
-    // 'max-len': ['error', {
-    //   code: 120,
-    //   tabWidth: 2,
-    //   ignoreComments: true,
-    //   ignoreTrailingComments: true,
-    //   ignoreUrls: true,
-    //   ignoreStrings: true,
-    //   ignoreTemplateLiterals: true,
-    // }],
+    'max-len': ['error', {
+      code: 100,
+      tabWidth: 2,
+      ignoreComments: true,
+      ignoreTrailingComments: true,
+      ignoreUrls: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+    }],
     'object-curly-spacing': ['error', 'always', { objectsInObjects: false }],
     'object-curly-newline': ['error', { consistent: true }],
-    // FIXME: Remove require-v-for-key once it's possible to ignore rules in <template> blocks
-    'vue/require-v-for-key': 'off', // not strictly necessary BUT you need to know what you're doing
-    'vue/max-attributes-per-line': 'off', // easier to read in a single line
+    'vue/max-attributes-per-line': 'off', // often easier to read in a single line
   }
 };
