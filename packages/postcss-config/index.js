279b3b4fd71b3e58f7ee36caae59fb4dc0d56fa5
Max Milton <max@wearegenki.com>
2018-01-27 09:14:50 +1100

    Add minimal and standalone options to PostCSS config preset

--- packages/postcss-config/index.js
+++ packages/postcss-config/index.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const postcss = require('postcss');
-const cssImport = require('postcss-import');
+const atImport = require('postcss-import');
 const atVariables = require('postcss-at-rules-variables');
 const each = require('postcss-each');
 const mixins = require('postcss-mixins');
@@ -17,42 +17,60 @@ const autoprefixer = require('autoprefixer');
 const url = require('postcss-url');
 const cssstats = require('cssstats');
 
-const mixinsPath = path.join(
-  path.dirname(require.resolve('@wearegenki/css')),
-  'src/mixins',
-);
-
-// PostCSS presets plugin
+/** PostCSS configuration preset. */
 module.exports.default = postcss.plugin('postcss-config', (opts = {}) => {
-  const mixinsDir = opts.mixinsDir ? [opts.mixinsDir] : [];
-  mixinsDir.push(mixinsPath);
+  const mixinsDir = [];
 
-  return postcss()
-    .use(cssImport({ path: opts.importPath || ['src'] }))
-    .use(atVariables)
-    .use(each)
-    .use(mixins({ mixinsDir }))
-    .use(nested)
-    .use(customProperties)
-    .use(conditionals)
-    .use(customMedia)
-    .use(calc({ warnWhenCannotResolve: opts.verbose }))
-    .use(colorFunction)
-    .use(mediaQueryPacker)
-    .use(autoprefixer({ remove: false, flexbox: 'no-2009' }))
-    .use(url);
+  if (opts.minimal || !opts.standalone) {
+    mixinsDir.push(path.join(
+      path.dirname(require.resolve('@wearegenki/css')),
+      'src/mixins'
+    ));
+
+    if (opts.mixinsDir) {
+      mixinsDir.push(opts.mixinsDir);
+    }
+  }
+
+  return opts.minimal
+    // reduced features version
+    ? postcss()
+      .use(atImport)
+      .use(nested)
+      .use(customMedia)
+      .use(mediaQueryPacker)
+      .use(autoprefixer)
+
+    // full featured version
+    : postcss()
+      .use(atImport({ path: opts.importPath || ['src'] }))
+      .use(atVariables)
+      .use(each)
+      .use(mixins({ mixinsDir }))
+      .use(nested)
+      .use(customProperties)
+      .use(conditionals)
+      .use(customMedia)
+      .use(calc({ warnWhenCannotResolve: opts.verbose }))
+      .use(colorFunction)
+      .use(mediaQueryPacker)
+      .use(autoprefixer({ remove: false, flexbox: 'no-2009' }))
+      .use(url);
 });
 
-// generate stats about the CSS
-// REF: https://github.com/cssstats/core
+/**
+ * Generate stats about CSS.
+ * @see https://github.com/cssstats/core
+ * @param {string} css The CSS to evaluate.
+ */
 // FIXME: Finish this implementation and write a usage guide
-module.exports.stats = function getStats(css) {
+module.exports.getStats = function getStats(css) {
   postcss()
     .use(cssstats) // TODO: If this doesn't work then use cssstats()
     .process(css)
     .then((result) => {
       result.messages.forEach((message) => {
-        console.log(message); // eslint-disable-line no-console
+        console.log(message);
       });
     });
 };
