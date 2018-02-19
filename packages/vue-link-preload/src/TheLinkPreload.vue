1099dc3dda17c4925f7f5344dc6b2eac6a9737fc
Max Milton <max@wearegenki.com>
2018-02-19 16:09:52 +1100

    vue-link-preload changes; minor performance improvements, label safe mangle names

--- packages/vue-link-preload/src/TheLinkPreload.vue
+++ packages/vue-link-preload/src/TheLinkPreload.vue
@@ -15,52 +15,47 @@ import futurelink from 'futurelink';
 export default {
   name: 'TheLinkPreload',
   data: () => ({
-    load: undefined,
+    load: null,
     options: {
       links: [],
     },
   }),
   mounted() {
-    // Don't run on the server
-    if (typeof window === 'undefined') {
-      return;
-    }
+    // don't run on server
+    if (typeof window === 'undefined') return;
 
-    // Don't run on mobile
-    if ('ontouchstart' in window) {
-      return;
-    }
+    // don't run on mobile
+    if ('ontouchstart' in window) return;
 
     const loaded = [this.$route.path];
 
     this.options.future = (link) => {
       const href = link.getAttribute('href');
 
-      if (loaded.includes(href)) {
-        return;
-      }
+      if (loaded.includes(href)) return;
 
       loaded.push(href);
 
-      // this.$emit('preload', href);
-
-      const resolved = this.$router.resolve(href);
-      const { matched } = resolved.resolved;
+      const resolver = this.$router.resolve(href);
+      const { matched } = resolver.resolved;
       this.load = matched[matched.length - 1].components.default;
     };
 
     futurelink(this.options);
 
-    this.init();
-    this.$router.afterEach(this.init);
+    this.__init();
+    this.$router.afterEach(this.__init);
   },
   methods: {
-    init() {
-      this.$nextTick(() => {
-        const links = document.querySelectorAll('a[href^="/"');
-        // Populate this.options.links {Array} from links {nodeList}
-        for (let i = -1, l = links.length; ++i !== l; this.options.links[i] = links[i]); // eslint-disable-line no-plusplus
-      });
+    __init() {
+      // no need to use $nextTick since it doesn't need to be immediate
+      setTimeout(() => {
+        const links = document.querySelectorAll('a[href^="/"]');
+
+        // fancy high performance way to convert nodeList into Array
+        // eslint-disable-next-line no-plusplus
+        for (let i = -1, l = links.length; ++i !== l; this.options.links[i] = links[i]);
+      }, 0);
     },
   },
 };
