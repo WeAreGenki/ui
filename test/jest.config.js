ea2c9439e00087666e78d480468bd87fbb2e40e8
Max Milton <max@wearegenki.com>
2018-03-07 10:33:55 +1100

    Move Jest config from /packages/test into /test

--- test/jest.config.js
+++ test/jest.config.js
@@ -2,16 +2,36 @@
 
 const path = require('path');
 
-// const libDir = '<rootDir>/node_modules/@wearegenki/test/lib/vue';
+// const libDir = '<rootDir>/packages/test/lib';
+const puppeteer = {
+  globalSetup: '<rootDir>/packages/test/lib/puppeteer/setup.js',
+  globalTeardown: '<rootDir>/packages/test/lib/puppeteer/teardown.js',
+  testEnvironment: '<rootDir>/packages/test/lib/puppeteer/environment.js',
+};
 
 module.exports = {
   preset: '@wearegenki/test',
   rootDir: path.resolve(__dirname, '..'),
   coverageDirectory: '<rootDir>/test/coverage',
+
   // resolver: `${libDir}/resolver.js`,
   // transform: {
   //   // '^.+\\.jsx?$': 'babel-jest', // don't use babel to transpile
   //   // '^.+\\.(html|marko)$': `${libDir}/transform.js`,
   // },
   // browser: true, // use browser field in package.json
+
+  // projects: ['<rootDir>', '<rootDir>/packages/*'],
+  projects: [
+    '<rootDir>',
+    '<rootDir>/packages/*',
+    // {
+    //   displayName: 'root',
+    // },
+    {
+      // displayName: '@wearegenki/test',
+      testMatch: ['<rootDir>/packages/test/__tests__/*.js'],
+      ...puppeteer,
+    },
+  ],
 };
