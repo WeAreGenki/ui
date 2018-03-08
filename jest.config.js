9dd62449efc3d45817a3491c86eeaf62d0a3882b
Max Milton <max@wearegenki.com>
2018-03-08 20:34:51 +1100

    Fix initial working test setup

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 'use strict'; // eslint-disable-line
 
+const presetConfig = require('@wearegenki/test/jest-preset'); // eslint-disable-line import/no-extraneous-dependencies
+
 module.exports = {
   preset: '@wearegenki/test',
 
@@ -10,33 +12,35 @@ module.exports = {
   globalTeardown: '<rootDir>/packages/test/lib/puppeteer/teardown.js',
   // testEnvironment: '<rootDir>/packages/test/lib/puppeteer/environment.js',
 
+  testPathIgnorePatterns: [
+    ...presetConfig.testPathIgnorePatterns,
+    '<rootDir>/packages/*',
+  ],
+
   // FIXME: This needs to be relative to each package
   // moduleNameMapper: {
   //   '^@/(.*)$': '<rootDir>/src/$1',
   // },
 
-  // FIXME: Need a way to separate out Vue, Marko, etc. test setups
-  // snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
-  // transform: {
-  //   '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
-  //   '^.+\\.(marko)$':
-  //     '<rootDir>/packages/test/lib/marko/transform.js',
-  //   '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
-  // },
+  // FIXME: Need a way to separate out Vue, Marko, and Node test setups (?)
+  snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(marko)$': '<rootDir>/packages/test/lib/marko/transform.js',
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
+  },
 
   projects: [
-    // '<rootDir>',
+    // XXX: Manually list each package until they all have tests then use a glob to match
+    // '<rootDir>/packages/*',
     '<rootDir>/packages/test',
-    //   // '<rootDir>/packages/*',
-    //   {
-    //     displayName: 'test',
+    {
+      displayName: 'test',
 
-    //     // TODO: Can we get away with not using puppeteer? Or maybe just use it for
-    //     // more e2e style test? The performance cost can be a bit much for this type
-    //     // of repo where there's a lot of small tests.
-    //     // globalSetup: '<rootDir>/packages/test/lib/puppeteer/setup.js',
-    //     // globalTeardown: '<rootDir>/packages/test/lib/puppeteer/teardown.js',
-    //     // testEnvironment: '<rootDir>/packages/test/lib/puppeteer/environment.js',
-    //   },
+      testPathIgnorePatterns: [
+        ...presetConfig.testPathIgnorePatterns,
+        '<rootDir>/packages/*',
+      ],
+    },
   ],
 };
