238ac8744f570078656cec2b66ba988e433b1039
Max Milton <max@wearegenki.com>
2018-03-12 14:13:29 +1100

    Experimental setup for marko in jest

--- packages/test/lib/marko/helpers.js
+++ packages/test/lib/marko/helpers.js
@@ -1,3 +1,18 @@
+/* eslint-disable */
+
 'use strict'; // eslint-disable-line
 
-module.exports = {};
+const lasso = require('lasso');
+const template = require('./template');
+
+async function mount() {
+  try {
+    // x
+  } catch (err) {
+    throw err;
+  }
+}
+
+module.exports = {
+  mount,
+};
