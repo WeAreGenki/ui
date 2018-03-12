b5373fad68eef089d8b3af4718ff82ed48a3ceb5
Max Milton <max@wearegenki.com>
2018-03-12 14:13:06 +1100

    Move puppeteer setup into jest preset

--- jest.config.js
+++ jest.config.js
@@ -4,14 +4,6 @@ const preset = require('@wearegenki/test/jest-preset'); // eslint-disable-line i
 
 module.exports = {
   preset: '@wearegenki/test',
-  globalSetup: 'jest-environment-puppeteer/setup',
-  globalTeardown: 'jest-environment-puppeteer/teardown',
-
-  // FIXME: This needs to be relative to each package
-  // moduleNameMapper: {
-  //   '^@/(.*)$': '<rootDir>/src/$1',
-  // },
-
   projects: [
     // XXX: Manually list each package until they all have tests then use a glob to match
     // '<rootDir>/packages/*',
