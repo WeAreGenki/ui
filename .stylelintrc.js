fa55b5170b53319be18947c8d323251b958c7989
Max Milton <max@wearegenki.com>
2018-03-15 14:47:34 +1100

    Workaround for stylelint bug

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,7 +5,8 @@
 module.exports = {
   extends: '@wearegenki/stylelint-config',
 
-  // rules: {
-  //   'selector-max-id': null, // fine for performance IF you know what you're doing
-  // },
+  rules: {
+    //   'selector-max-id': null, // fine for performance IF you know what you're doing
+    'string-no-newline': null, // FIXME: Is currently throwing false positives
+  },
 };
