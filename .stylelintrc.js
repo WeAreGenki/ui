3884e90679faa2b865ddf96ae3f4b210e33ff31d
Max Milton <max@wearegenki.com>
2018-03-15 14:48:12 +1100

    Remove space

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: '@wearegenki/stylelint-config',
 
   rules: {
-    //   'selector-max-id': null, // fine for performance IF you know what you're doing
+    // 'selector-max-id': null, // fine for performance IF you know what you're doing
     'string-no-newline': null, // FIXME: Is currently throwing false positives
   },
 };
