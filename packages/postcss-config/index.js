62a10b41c8bcb37aeb92e6c94e7f2814e0aa8280
Max Milton <max@wearegenki.com>
2018-03-25 09:48:40 +1100

    Fix some 'use strict'

--- packages/postcss-config/index.js
+++ packages/postcss-config/index.js
@@ -1,4 +1,4 @@
-'use-strict';
+'use strict'; // eslint-disable-line
 
 const path = require('path');
 const postcss = require('postcss');
--- packages/ui/index.js
+++ packages/ui/index.js
@@ -1,6 +1,4 @@
-// TODO: Add a way to install all Vue plugins directly from this package
-
-'use-strict';
+'use strict'; // eslint-disable-line
 
 const uiPostcssPreset = require('@wearegenki/postcss-config');
 
